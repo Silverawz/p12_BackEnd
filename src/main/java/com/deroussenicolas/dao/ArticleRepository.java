8013e84a1d6f2934d5b24ade3ffd1f386df7c3b9
silverawz <deroussenicolas@gmail.com>
2020-09-01 16:38:00 +0200

    pageable almost done for articles

--- src/main/java/com/deroussenicolas/dao/ArticleRepository.java
+++ src/main/java/com/deroussenicolas/dao/ArticleRepository.java
@@ -2,8 +2,11 @@ package com.deroussenicolas.dao;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.deroussenicolas.entities.Article;
@@ -15,7 +18,7 @@ import com.deroussenicolas.entities.Category;
  * 
  */
 @Repository
-public interface ArticleRepository extends JpaRepository <Article, Long> {
+public interface ArticleRepository extends PagingAndSortingRepository<Article, Long>, JpaRepository <Article, Long> {
 	
 
 	
@@ -39,4 +42,15 @@ public interface ArticleRepository extends JpaRepository <Article, Long> {
 
 	@Query(nativeQuery=true, value = "SELECT * FROM Article a JOIN Category_has_article b ON a.id_article=b.Article_id_article where a.user_id_user=?1 order by a.date desc")
 	List<Article> findAllArticlesFromUser(Long user_id);
+
+	
+	
+	
+	@Query(nativeQuery=true, value = "SELECT * FROM Article a JOIN Category_has_article b ON a.id_article=b.Article_id_article where b.category_id_category=?1 and a.active=?2 order by a.date desc", 
+			countQuery = "SELECT count(*) FROM Article a JOIN Category_has_article b ON a.id_article=b.Article_id_article where b.category_id_category=?1 and a.active=?2 order by a.date desc")
+	Page<Article> getAllArticles(Long id, boolean active, Pageable pageable);
+
+	
+
+
 }
