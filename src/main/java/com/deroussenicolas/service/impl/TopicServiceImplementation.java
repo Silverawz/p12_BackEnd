b82868b706552e290e91a6d3ef281ad7811ee5e3
silverawz <deroussenicolas@gmail.com>
2020-09-18 13:14:02 +0200

    last change, tests done, clean code

--- src/main/java/com/deroussenicolas/service/impl/TopicServiceImplementation.java
+++ src/main/java/com/deroussenicolas/service/impl/TopicServiceImplementation.java
@@ -7,16 +7,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.deroussenicolas.dao.CategoryRepository;
 import com.deroussenicolas.dao.TopicRepository;
 import com.deroussenicolas.entities.Topic;
 import com.deroussenicolas.exception.InvalidTopicException;
 import com.deroussenicolas.service.CategoryService;
 import com.deroussenicolas.service.TopicService;
-import org.springframework.data.domain.Pageable;
 /**
  * implements TopicService
  * 
